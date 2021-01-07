90b0d7633b6028ac1c47883db488b9cf54c85017
tristan-tsl <tanshilinmail@gmail.com>
2021-01-07 20:29:09 +0800

    抽取sql指纹

--- component/mymysql.py
+++ component/mymysql.py
@@ -40,23 +40,80 @@ def execute(sql, parameters=None, is_batch_insert=False):
     return execute_result
 
 
-def extra_sql_content_2_template(sql_content):
+def extra_sql_template(sql_content):
     """
-    抽取SQL内容为SQL模板
+    抽取SQL模板(指纹)
     :param sql_content: SQL内容
     :return:
     """
-    sql_template = sql_content
     """
     抽取SQL模板的思路: 
     替换
         > < = ! 
-            'xxx'   -> ?
-            数字    -> ?  
-            ()      -> 
-        in (xxx)  -> in(?)
+            'xxx'                        -> ?
+            特殊字符+数字开头的+特殊字符   -> 特殊字符+?+特殊字符  
+        in (xxx)                         -> in(?)
     """
-
-
+    print("=" * 200)
+    print("sql_content: %s" % sql_content)
+    sql_template = ''
+    is_quote_start = False  # 是否开始引号
+    is_digit_start = False  # 是否开始数字
+    last_sql_content_item = ''
+    for item in sql_content:
+        # 从字符串中抽取引号部分为模板
+        if item == '\'':
+            is_quote_start = not is_quote_start
+            if not is_quote_start:
+                sql_template += '?'
+            continue
+        if is_quote_start:
+            continue
+        # 从字符串中抽取数字部分为模板
+        if item.isdigit():
+            if not last_sql_content_item.isdigit() and not last_sql_content_item.isalpha():
+                is_digit_start = True
+        else:
+            if is_digit_start:
+                is_digit_start = False
+                sql_template += '?'
+        if is_digit_start:
+            continue
+        sql_template += item
+        last_sql_content_item = item
+    if is_digit_start:
+        sql_template += '?'
+    # 从字符串中抽取in()部分中多个模板为单个模板
+    is_i_start = False
+    is_n_start = False
+    is_question_start = False
+    sql_template_back = ''
+    for item in sql_template:
+        if item.lower() == 'i':
+            is_i_start = True
+        else:
+            if is_i_start:
+                if item.lower() == 'n':
+                    is_i_start = False
+                    is_n_start = True
+                else:
+                    is_i_start = False
+            else:
+                if is_n_start:
+                    # 是否应该考虑 hint段? 暂时不考虑
+                    if item.isalpha():
+                        is_n_start = False
+                    elif item == '?':
+                        is_n_start = False
+                        is_question_start = True
+                else:
+                    if is_question_start:
+                        if item in ['?', ',', ' ']:
+                            continue
+                        elif item == ")":
+                            is_question_start = False
+        sql_template_back += item
+    sql_template = sql_template_back
+    print("sql_template: %s" % sql_template)
 
     return sql_template
--- service/sync_polardb_slow_log.py
+++ service/sync_polardb_slow_log.py
@@ -86,34 +86,38 @@ def store_response_result(resp_result):
         # 2021-01-05T00:03:01Z
         data_timestamp = int(
             time.mktime(time.strptime(execution_start_time[:-6] + "00:00Z", '%Y-%m-%dT%H:%M:%SZ')))  # 丢失掉秒钟精度
+        # 生成sql模板
+        sql_template = mymysql.extra_sql_template(sql_text)
         parameters.append([
             db_cluster_id, db_name,
             item["DBNodeId"], execution_start_time, host_address,
             item["LockTimes"], item["QueryTimes"], item["ParseRowCounts"],
-            item["ReturnRowCounts"], sql_text, data_timestamp
+            item["ReturnRowCounts"], sql_text, data_timestamp,
+            sql_template
         ])
 
     mymysql.execute("""
-    INSERT INTO `polardb_slow_log`(`db_cluster_id`, `db_name`
+    INSERT INTO `polardb_slow_log_2`(`db_cluster_id`, `db_name`
     , `db_node_id`, `execution_start_time`, `host_address`
     , `lock_times`, `query_times`, `parse_row_counts`
-    , `return_row_counts`, `sql_text`, `data_timestamp`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    , `return_row_counts`, `sql_text`, `data_timestamp`
+    , `sql_template`
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """, parameters, True)
-    # TODO 从SQL语句中抽取语句模块, 合并多条语句
 
 
 
 def start():
     day_interval = -1
     start_datetime = (datetime.today() + timedelta(day_interval)).strftime("%Y-%m-%d") + "T00:00Z"
-    print(start_datetime)
+    # print(start_datetime)
     end_datetime = (datetime.today() + timedelta(day_interval + 1)).strftime("%Y-%m-%d") + "T00:00Z"
     page_number = 1
     page_size = 100
     while True:
         resp_result = request_slow_log(db_cluster_id, start_datetime, end_datetime, page_number, page_size)
         store_response_result(resp_result)
-        print(resp_result)
+        # print(resp_result)
         if page_number * page_size > resp_result["TotalRecordCount"]:
             break
         else:
